e87c78b801db104e1d537a94a0244ec00b0c24ae
AK <kasssalex@gmail.com>
2018-01-20 14:38:29 +0100

    Move out a UI searchbox dependency

--- src/app/basic-data-services/config.service.ts
+++ src/app/basic-data-services/config.service.ts
@@ -24,33 +24,4 @@ export class ConfigService {
         return true
     }
 
-
-    private listSaveMap= new Map<string, any>()
-
-    private getMapElement(listType: string): any {
-        if (!this.listSaveMap.has(listType)) this.listSaveMap.set(listType, {searchTxt:'', nbHits: -1})
-        return this.listSaveMap.get(listType)
-    }
-
-    listSaveSearchText(listType: string, searchTxt: string) {
-        var elem= this.getMapElement(listType)
-        elem.searchTxt= searchTxt
-        this.listSaveMap.set(listType, elem)
-    }
-
-    listGetSearchText(listType: string) {
-        return this.getMapElement(listType).searchTxt
-    }
-
-    listSaveNbHits(listType: string, nbHits: number) {
-        var elem= this.getMapElement(listType)
-        elem.nbHits= nbHits
-        this.listSaveMap.set(listType, elem)
-    }
-
-    listGetNbHits(listType: string, defaultNb:number) {
-        var n= this.getMapElement(listType).nbHits
-        return n != -1 ? n : defaultNb
-    }
-
 }
