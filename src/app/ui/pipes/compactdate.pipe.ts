beb2313e12dc02f515a89b2bcdc6d6d42ec1a9c0
AK <kasssalex@gmail.com>
2018-02-02 19:47:10 +0100

    Add translate-localization into custom date pipes (not perfect yet: doesn't use observables)
    When already open and change language: no language switch of date; only when opening component is locale taken into account

--- src/app/ui/pipes/compactdate.pipe.ts
+++ src/app/ui/pipes/compactdate.pipe.ts
@@ -1,9 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from "moment"
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core'
 
 @Pipe({name: 'compactDate'})
 export class CompactDatePipe implements PipeTransform {
+  constructor(private translateService: TranslateService )  {
+
+  }
+
+
   transform(date, param) {
+    moment.locale(this.translateService.currentLang);
+    
     if (!moment(date, 'DD/MM/YYYY HH:mm:ss').isValid()) return date
     return moment(date, 'DD/MM/YYYY HH:mm:ss').format('l');    
   }
--- src/app/ui/pipes/shortdate.pipe.ts
+++ src/app/ui/pipes/shortdate.pipe.ts
@@ -1,9 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from "moment"
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core'
+
 
 @Pipe({name: 'shortDate'})
 export class ShortDatePipe implements PipeTransform {
+  constructor(private translateService: TranslateService )  {
+
+  }
+
   transform(date, param) {
+    moment.locale(this.translateService.currentLang);
     if (!moment(date, 'DD/MM/YYYY HH:mm:ss').isValid()) return date
     return moment(date, 'DD/MM/YYYY HH:mm:ss').format('LL');    
   }
