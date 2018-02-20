2844e60c6135b44b57422632ca792759850124db
AK <kasssalex@gmail.com>
2018-02-20 13:50:46 +0100

    always enable Reset button  +  pasting e-mails: clean parasites

--- src/app/ui/forms/form-generic.component.ts
+++ src/app/ui/forms/form-generic.component.ts
@@ -14,8 +14,8 @@ import { Observable } from 'rxjs/Observable';
 
 export class FormGenericComponent implements OnInit {
 
-    public newForm: FormGroup= new FormGroup({});
-    public errorMessage: string= ''
+    public newForm: FormGroup = new FormGroup({});
+    public errorMessage: string = ''
 
     constructor(private formBuilder: FormBuilder) {
     }
@@ -25,31 +25,33 @@ export class FormGenericComponent implements OnInit {
 
     FormItemType: typeof FormItemType = FormItemType;        // Necessary: see https://stackoverflow.com/questions/35923744/pass-enums-in-angular2-view-templates 
 
-    @Input() structure: FormItemStructure[]= []
-    @Input() labelColumns: number= 3
-    @Input() dataColumns: number= 4
-    @Input() primaryControlName: string= 'name'
-    @Input() primaryDataObservable: Observable<any>= undefined
-    @Output() formSaved = new EventEmitter()    
+    @Input() structure: FormItemStructure[] = []
+    @Input() labelColumns: number = 3
+    @Input() dataColumns: number = 4
+    @Input() primaryControlName: string = 'name'
+    @Input() primaryDataObservable: Observable<any> = undefined
+    @Output() formSaved = new EventEmitter()
 
-    @Output() gigaControlChanged = new EventEmitter()    
-    
-    public validatorColumns: number= 5
+    @Output() gigaControlChanged = new EventEmitter()
 
-    showValidation: boolean= false
+    public validatorColumns: number = 5
+
+    showValidation: boolean = false
 
     public isPageRunning: boolean = true
 
+
+
     ngOnInit(): void {
-        const emailRegex = /^\s*[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}\s*$/i;
         const telRegex = /^\s*[\+0]{1}[0-9]{7,20}\s*$/im
+        const emailRegex = /^\s*([0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3})\s*$/i;
         //const priceRegEx = `^\\d+(.\\d*)?$`
 
-        this.validatorColumns= 12 - this.labelColumns - this.dataColumns
+        this.validatorColumns = 12 - this.labelColumns - this.dataColumns
         this.structure.filter(s => s.isStandard()).forEach(s => {
-            var validators= []
+            var validators = []
             if (s.isRequired) validators.push(Validators.required)
-            if (s.minimalLength) validators.push(Validators.minLength(s.minimalLength)) 
+            if (s.minimalLength) validators.push(Validators.minLength(s.minimalLength))
             if (s.isEmail) validators.push(Validators.pattern(emailRegex))
             if (s.isTelephone) validators.push(Validators.pattern(telRegex))
             this.newForm.addControl(s.name, new FormControl(s.value, validators))
@@ -58,7 +60,7 @@ export class FormGenericComponent implements OnInit {
         this.initCheck()
     }
 
-    public alreadyInDb: boolean= false
+    public alreadyInDb: boolean = false
 
     private initCheck() {
         if (!this.primaryDataObservable || !this.newForm.controls[this.primaryControlName]) return
@@ -74,51 +76,65 @@ export class FormGenericComponent implements OnInit {
     ngOnDestroy(): void {
         this.isPageRunning = false
     }
-    
+
     save(formValue, isValid) {
-        this.showValidation= true
+        this.showValidation = true
         if (!isValid) return
-        var retObj= {}
+        var retObj = {}
         this.structure.filter(s => s.isStandard()).forEach(s => {
-            var value= formValue[s.name]
-            if (s.type === FormItemType.InputNumber) value= +value
-            if (s.type === FormItemType.InputText) value= (value || '').trim()
-            if (s.type === FormItemType.InputCheckbox) value= value !== '' && value !== null
-            retObj[s.name]= value
+            var value = formValue[s.name]
+            if (s.type === FormItemType.InputNumber) value = +value
+            if (s.type === FormItemType.InputText) value = (value || '').trim()
+            if (s.type === FormItemType.InputCheckbox) value = value !== '' && value !== null
+            retObj[s.name] = value
         })
         this.structure.filter(s => !s.isStandard()).forEach(s => {
-            retObj[s.name]= s.value
+            retObj[s.name] = s.value
         })
-        retObj['setError']= (msg) => {this.errorMessage=msg}
-        retObj['setSuccess']= (msg) => {this.reset()}
+        retObj['setError'] = (msg) => { this.errorMessage = msg }
+        retObj['setSuccess'] = (msg) => { this.reset() }
         this.formSaved.next(retObj)
     }
 
     reset() {
-        this.showValidation= false
+        this.showValidation = false
         this.selectorViewChildren.toArray().forEach(s => s.emptyContent())
         this.datesViewChildren.toArray().forEach(s => s.emptyContent())
         this.newForm.reset();
         this.initCheck()
-    }    
+    }
 
     isInvalid(controlName): boolean {
-        var control= this.newForm.controls[controlName]
+        var control = this.newForm.controls[controlName]
         if (!control) return true
         return !control.valid
     }
 
     fieldChanged(controlName, value) {
-        var control= this.structure.filter(s => s.name === controlName)[0]
-        if (control) control.value= value
+        var control = this.structure.filter(s => s.name === controlName)[0]
+        if (control) control.value = value
         this.gigaControlChanged.next({
             name: controlName,
             value: value,
             fnChangeControl: (_controlName, _value) => {
-                var obj: any= {}
-                obj[_controlName]= _value
+                var obj: any = {}
+                obj[_controlName] = _value
                 this.newForm.patchValue(obj)
             }
         })
+    }
+
+    onPaste(item, e: any) {
+        const emailRegex = /[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}/i;
+        if (item.isEmail) { // clean e-mail address, as people tend to copy paste e-mail addresses
+            let content: string = e.clipboardData.getData('text/plain')
+            var test = content.match(emailRegex)
+            if (test) {
+                if (test[0] !== content) {
+                    e.preventDefault()
+                    e.target.value = test[0]
+                }
+            }
+        }
     }
 }
